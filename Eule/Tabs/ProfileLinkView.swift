a94d2e9aaf7795d0fff286ae1a54352cc6e5d67a
aannuuj <hariompalkar@gmail.com>
2020-11-26 01:04:51 +0530

    feat : ShareSheet View

--- Eule/Tabs/ProfileLinkView.swift
+++ Eule/Tabs/ProfileLinkView.swift
@@ -9,39 +9,47 @@
 import SwiftUI
 
 struct ProfileLinkView: View {
-
-       public let title: String
-       public let subtitle: String
-       public let imageName: String
-      
-    var body: some View {
-        Button(action: {}) {
-            HStack(alignment: .center, spacing: 10) {
-                       Image(imageName)
-                         
-                       
-                       VStack(alignment: .leading, spacing: 8) {
-                           Text(title)
-                               .font(.EuleLabel)
-                               .foregroundColor(.primary)
-                           Text(subtitle)
-                               .font(.EuleTitle)
-                               .foregroundColor(.secondary)
-                       }
-                     Spacer()
-                    Image(systemName: "chevron.right")
-                      
-                       
-                   }
-                   .padding([.leading, .bottom, .leading, .trailing])
-               }
-               .buttonStyle(PlainButtonStyle())
-           }
+    public let action: () -> ()
+    public let title: String
+    public let subtitle: String
+    public let imageName: String
+    
+    
+    public init(
+        action: @escaping () -> () = { },
+        title: String,
+        subtitle: String,
+        imageName: String
+        ) {
+        self.action = action
+        self.title = title
+        self.subtitle = subtitle
+        self.imageName = imageName
     }
-
-
-struct ProfileLinkView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileLinkView( title: "", subtitle: "", imageName: "")
+    
+    var body: some View {
+        Button(action: action) {
+            HStack(alignment: .center, spacing: 10) {
+                Image(imageName)
+                
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(title)
+                        .font(.EuleLabel)
+                        .foregroundColor(.primary)
+                    Text(subtitle)
+                        .font(.EuleTitle)
+                        .foregroundColor(.secondary)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            .padding([.leading, .bottom, .leading, .trailing])
+        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
+
+
+
+
